7d442729c3e061c42ed86de2c0c71d778bbe332e
kervingc <kegarciaca@ittepic.edu.mx>
2022-12-25 17:38:44 -0800

    Feat: Conditionals are added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,20 +13,25 @@ const Expenses = (props) => {
   const filteredExpenses = props.items.filter(
     (x) => x.date.getFullYear().toString() === filteredYear
   );
+
+  let expensesContent = <p>No expenses found</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((item, index) => (
+      <ExpenseItem
+        key={item.id}
+        title={item.title}
+        amount={item.amount}
+        date={item.date}
+      />
+    ));
+  }
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.map((item, index) => (
-        <ExpenseItem
-          key={item.id}
-          title={item.title}
-          amount={item.amount}
-          date={item.date}
-        />
-      ))}
+      {expensesContent}
     </Card>
   );
 };
